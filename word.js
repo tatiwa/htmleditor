976e138b342a446a56fe1cfe25cba3087a81c5be
Tat Iwa <tat@tat.org>
2012-10-29 14:01:20 -0700

    GET path works properly using cheerio to parse the template html source. Next POST to save to file.

--- word.js
+++ word.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
 var Encoder = require('node-html-encoder').Encoder;
-
+var cheerio = require("cheerio");
  
 http.createServer(function (request, response) {
  
@@ -17,6 +17,9 @@ http.createServer(function (request, response) {
         var encoder = new Encoder('entity');
         var htmlSourcePre = fs.readFileSync("tinymce/word_pre.html", "utf8");
         var htmlSourcePost = fs.readFileSync("tinymce/word_post.html", "utf8");
+        $ = cheerio.load(fs.readFileSync("tinymce/word.html", "utf8"));
+        //console.log($("html").html());
+
         var htmlText;
         if(request.method=='POST') {
             var body='';
@@ -34,14 +37,28 @@ http.createServer(function (request, response) {
             });
         } else if(request.method=='GET') {
             request.on('end',function(){
+                var word = cheerio.load(fs.readFileSync("tinymce/word.html", "utf8"));
                 console.log(requestparse.query.file);
-                var cheerio = require("cheerio"),
-                    $ = cheerio.load(fs.readFileSync(requestparse.query.file, "utf8"));
-                console.log($("body").html());
-                response.writeHead(200, 'Content-Type: text/html' );
-                response.write(htmlSourcePre, "utf8");
-                response.write(encoder.htmlEncode($("body").html()), "utf8");
-                response.end(htmlSourcePost, "utf8");
+                fs.exists(requestparse.query.file, function(exists) {
+                    if (exists) {
+                        var text = encoder.htmlEncode(
+                            cheerio.load(fs.readFileSync(requestparse.query.file, "utf8"))("body").html()
+                        );
+                        console.log(text);    
+                        word("textarea").replaceWith(
+                        	'<textarea id="elm1" name="elm1" rows="15" cols="80" style="width: 100%">' +
+                        	text + '</textarea>'
+                        );
+                        console.log(word("html").html());
+                        response.writeHead(200, 'Content-Type: text/html' );
+                        response.end(word("html").html(), "utf8");
+                            
+                    } else {
+                        response.writeHead(200, 'Content-Type: text/html' );
+                        response.end(word("html").html(), "utf8");
+                    }
+        	    });
+
             });
         }
     } else {
