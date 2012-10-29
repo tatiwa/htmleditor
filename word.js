fc23f3acad6753dcc47f20fc7b80db23e1e1ada6
Tat Iwa <tat@tat.org>
2012-10-29 01:46:09 -0700

    snapshot. Use cheerio to extract the body of specified THML doc. Next: use POST to save the file.

--- word.js
+++ word.js
@@ -8,31 +8,41 @@ var Encoder = require('node-html-encoder').Encoder;
 http.createServer(function (request, response) {
  
     console.log("request %s", request.url);
-    var filePath = '.' + require('url').parse(request.url, true).pathname;
+    var requestparse = require('url').parse(request.url, true);
+    var filePath = '.' + requestparse.pathname;
     if (filePath == './')
         filePath = './word.html';
 
     if (filePath == './word.html') {
+        var encoder = new Encoder('entity');
+        var htmlSourcePre = fs.readFileSync("tinymce/word_pre.html", "utf8");
+        var htmlSourcePost = fs.readFileSync("tinymce/word_post.html", "utf8");
+        var htmlText;
         if(request.method=='POST') {
-                var body='';
-                request.on('data', function (data) {
-                   body +=data;
-                });
-                request.on('end',function(){
-                   var POST =  qs.parse(body);
-                   console.log(POST);
-                   response.writeHead(200, 'Content-Type: text/html');
-                   response.end(POST['elm1'], 'utf-8');
-                });
-        } else if(request.method=='GET') {
-                var encoder = new Encoder('entity');
-                var htmlSourcePre = fs.readFileSync("tinymce/word_pre.html", "utf8");
-                var htmlText = fs.readFileSync("index.html", "utf8");
-                var htmlSourcePost = fs.readFileSync("tinymce/word_post.html", "utf8");
+            var body='';
+            request.on('data', function (data) {
+               body +=data;
+            });
+            request.on('end',function(){
+                var POST =  qs.parse(body);
+                htmlText = POST.elm1;
+                console.log(encoder.htmlEncode(htmlText));
                 response.writeHead(200, 'Content-Type: text/html' );
                 response.write(htmlSourcePre, "utf8");
                 response.write(encoder.htmlEncode(htmlText), "utf8");
                 response.end(htmlSourcePost, "utf8");
+            });
+        } else if(request.method=='GET') {
+            request.on('end',function(){
+                console.log(requestparse.query.file);
+                var cheerio = require("cheerio"),
+                    $ = cheerio.load(fs.readFileSync(requestparse.query.file, "utf8"));
+                console.log($("body").html());
+                response.writeHead(200, 'Content-Type: text/html' );
+                response.write(htmlSourcePre, "utf8");
+                response.write(encoder.htmlEncode($("body").html()), "utf8");
+                response.end(htmlSourcePost, "utf8");
+            });
         }
     } else {
 	    var extname = path.extname(filePath);
@@ -53,7 +63,6 @@ http.createServer(function (request, response) {
 	    }
 	     
 	    fs.exists(filePath, function(exists) {
-	     
 	        if (exists) {
 	            fs.readFile(filePath, function(error, content) {
 	                if (error) {
