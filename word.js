b5329eb3ecc31bf32bdabfea9d71e186a65429e3
tat iwamoto <tat.iwamoto@ntd.nintendo.com>
2013-01-15 02:04:13 -0800

    taking snapshot at half baked mess

--- word.js
+++ word.js
@@ -15,19 +15,21 @@ http.createServer(function (request, response) {
 
     if (filePath == './word.html') {
         var encoder = new Encoder('entity');
-        var word = cheerio.load(fs.readFileSync("nodescripts/word.html", "utf8"));
+        var word = cheerio.load(fs.readFileSync("htmleditor/word.html", "utf8"));
         var targetFile = requestparse.query.file;
         // Form action change
+        /*
         word("form")[0].attribs.action += '?file=' + targetFile;
         console.log(word("form")[0].attribs.action);
+        */
 
-        if(request.method=='POST') {
+        if(request.method=='POST') { // Push data to server
             var body='';
             request.on('data', function (data) {
                body +=data;
             });
             request.on('end',function(){
-                console.log('POST');    
+                console.log('word.html POST');    
                 var POST =  qs.parse(body);
                 console.log(POST);    
                 var htmlText = POST.elm1;
@@ -53,34 +55,75 @@ http.createServer(function (request, response) {
                     response.end(word("html").html(), "utf8");
         	    });
         	});
-        } else if(request.method=='GET') {
+        } else if(request.method=='GET') { // Page load
             request.on('end',function(){
-                console.log('GET');    
-                
+                console.log('word.html GET');    
+                response.writeHead(200, 'Content-Type: text/html' );
+                response.end(fs.readFileSync("htmleditor/word.html", "utf8"), "utf8");
+            });
+        }
+    } else if (filePath == './test.html') { // handle test POST request
+        var targetFile = requestparse.query.file;
+
+        if(request.method=='POST') { // Push data to server
+            var body='';
+            request.on('data', function (data) {
+                body +=data;
+            });
+            request.on('end',function(){
+                console.log('test.html POST');    
+/*
+                console.log("\nPOST begin\n\n-----\n"+targetFile+"\n-----\n\n");
+                console.log(body);
+                console.log("\nPOST end\n\n-----\n"+targetFile+"\n-----\n\n");
+
                 fs.exists(targetFile, function(exists) {
                     if (exists) {
-                        console.log("Targetfile: " + targetFile);
-                        var htmlText = cheerio.load(fs.readFileSync(targetFile, "utf8"));
-                        // console.log(htmlText.html());
-                        word("title").replaceWith(
-                        	'<title>' + htmlText("title").html() + '</textarea>'
-                        );
-                        var text = encoder.htmlEncode(htmlText("body").html());
-                        console.log(text);    
-                        // Textarea change
+                        console.log(targetFile);    
+                        var filetext = cheerio.load(fs.readFileSync(targetFile, "utf8"));
+
+                        // Replace body and save file
+                        filetext("body").replaceWith('<body>\n' + htmlText + '\n</body>');
+                        console.log(filetext.html());
+                        fs.writeFileSync(targetFile, filetext.html(), "utf8");
+
+                        // Modify browser window
                         word("textarea").replaceWith(
                         	'<textarea id="elm1" name="elm1" rows="15" cols="80" style="width: 100%">' +
-                        	text + '</textarea>'
+                        	htmlText + '</textarea>'
                         );
-                    }
+                    } 
                     response.writeHead(200, 'Content-Type: text/html' );
                     response.end(word("html").html(), "utf8");
+        	    });
+*/
+
+                fs.writeFileSync(targetFile, body, "utf8");
+                response.writeHead(200, 'Content-Type: text/html' );
+                response.end(body, "utf8");
+                
+        	});
+        } else if(request.method=='GET') { // Page load
+            request.on('end',function(){
+                console.log('test.html GET');    
+                
+                fs.exists(targetFile, function(exists) {
+                    
+                    if (!exists) {
+                        fs.createReadStream('htmleditor/empty.html').pipe(fs.createWriteStream(targetFile));
+                    }
+                    var filetext = cheerio.load(fs.readFileSync(targetFile, "utf8"));
+                    response.writeHead(200, 'Content-Type: text/html' );
+                    response.end(filetext("body"), "utf8");
+                    console.log(targetFile);
+                    console.log(filetext("body"));
                     
         	    });
 
             });
         }
-    } else {
+
+    } else { // Feed regular files
 	    var extname = path.extname(filePath);
 	    var contentType = 'text/html';
 	    switch (extname) {
