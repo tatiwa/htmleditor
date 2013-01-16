26eb478a7c13fbc7d615b76e704eb5b44f6a930d
tat iwamoto <tat.iwamoto@ntd.nintendo.com>
2013-01-15 17:26:42 -0800

    Cleaned up and working. Font size still behaving funny sometimes

--- word.js
+++ word.js
@@ -17,52 +17,15 @@ http.createServer(function (request, response) {
         var encoder = new Encoder('entity');
         var word = cheerio.load(fs.readFileSync("htmleditor/word.html", "utf8"));
         var targetFile = requestparse.query.file;
-        // Form action change
-        /*
-        word("form")[0].attribs.action += '?file=' + targetFile;
-        console.log(word("form")[0].attribs.action);
-        */
 
-        if(request.method=='POST') { // Push data to server
-            var body='';
-            request.on('data', function (data) {
-               body +=data;
-            });
-            request.on('end',function(){
-                console.log('word.html POST');    
-                var POST =  qs.parse(body);
-                console.log(POST);    
-                var htmlText = POST.elm1;
-                console.log(htmlText);
-
-                fs.exists(targetFile, function(exists) {
-                    if (exists) {
-                        console.log(targetFile);    
-                        var filetext = cheerio.load(fs.readFileSync(targetFile, "utf8"));
-
-                        // Replace body and save file
-                        filetext("body").replaceWith('<body>\n' + htmlText + '\n</body>');
-                        console.log(filetext.html());
-                        fs.writeFileSync(targetFile, filetext.html(), "utf8");
-
-                        // Modify browser window
-                        word("textarea").replaceWith(
-                        	'<textarea id="elm1" name="elm1" rows="15" cols="80" style="width: 100%">' +
-                        	htmlText + '</textarea>'
-                        );
-                    } 
-                    response.writeHead(200, 'Content-Type: text/html' );
-                    response.end(word("html").html(), "utf8");
-        	    });
-        	});
-        } else if(request.method=='GET') { // Page load
+        if(request.method=='GET') { // Page load
             request.on('end',function(){
                 console.log('word.html GET');    
                 response.writeHead(200, 'Content-Type: text/html' );
                 response.end(fs.readFileSync("htmleditor/word.html", "utf8"), "utf8");
             });
         }
-    } else if (filePath == './test.html') { // handle test POST request
+    } else if (filePath == './update.html') { // handle test POST request
         var targetFile = requestparse.query.file;
 
         if(request.method=='POST') { // Push data to server
@@ -71,32 +34,7 @@ http.createServer(function (request, response) {
                 body +=data;
             });
             request.on('end',function(){
-                console.log('test.html POST');    
-/*
-                console.log("\nPOST begin\n\n-----\n"+targetFile+"\n-----\n\n");
-                console.log(body);
-                console.log("\nPOST end\n\n-----\n"+targetFile+"\n-----\n\n");
-
-                fs.exists(targetFile, function(exists) {
-                    if (exists) {
-                        console.log(targetFile);    
-                        var filetext = cheerio.load(fs.readFileSync(targetFile, "utf8"));
-
-                        // Replace body and save file
-                        filetext("body").replaceWith('<body>\n' + htmlText + '\n</body>');
-                        console.log(filetext.html());
-                        fs.writeFileSync(targetFile, filetext.html(), "utf8");
-
-                        // Modify browser window
-                        word("textarea").replaceWith(
-                        	'<textarea id="elm1" name="elm1" rows="15" cols="80" style="width: 100%">' +
-                        	htmlText + '</textarea>'
-                        );
-                    } 
-                    response.writeHead(200, 'Content-Type: text/html' );
-                    response.end(word("html").html(), "utf8");
-        	    });
-*/
+                console.log('update.html POST');    
 
                 fs.writeFileSync(targetFile, body, "utf8");
                 response.writeHead(200, 'Content-Type: text/html' );
@@ -105,7 +43,7 @@ http.createServer(function (request, response) {
         	});
         } else if(request.method=='GET') { // Page load
             request.on('end',function(){
-                console.log('test.html GET');    
+                console.log('update.html GET');    
                 
                 fs.exists(targetFile, function(exists) {
                     if (!exists) {
